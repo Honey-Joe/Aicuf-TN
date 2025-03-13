8dfdd336b98a2c12de15a667e60dfd3e0744dd01
Honey-Joe <honeyjoe942@gmail.com>
2025-03-13 13:03:23 +0530

    commit

--- script.js
+++ script.js
@@ -11,21 +11,6 @@ const menuBtn = document.getElementById('menu-btn');
             document.getElementById('pdfViewer').classList.remove('hidden');
             document.getElementById('placeholderText').classList.add('hidden');
         }
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 1, // Default: 1 slide per view
-            spaceBetween: 20, // Space between slides
-            loop: true, // Enable infinite loop
-            autoplay: {
-                delay: 3000, // Auto-slide every 3 seconds
-                disableOnInteraction: false, // Continue autoplay after user interaction
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            breakpoints: {
-                640: { slidesPerView: 1 }, // Mobile
-                768: { slidesPerView: 2 }, // Tablet
-                1024: { slidesPerView: 3 } // Desktop
-            }
-        });
+       
+
+       
