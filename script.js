f8279f1f22ae64163ea99cf1dc5a743debfc896d
Honey-Joe <honeyjoe942@gmail.com>
2025-03-15 12:34:52 +0530

    updated the code

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const menuBtn = document.getElementById('menu-btn');
         });
 
 
-        function showPDF(pdfUrl) {
-            document.getElementById('pdfViewer').src = pdfUrl;
-            document.getElementById('pdfViewer').classList.remove('hidden');
-            document.getElementById('placeholderText').classList.add('hidden');
-        }
+        function showPDF(pdfPath) {
+            if (window.innerWidth < 1020) {
+              // On Mobile, open in a new tab
+              window.open(pdfPath, "_blank");
+              
+            } else {
+              // On Desktop, show inside iframe
+              document.getElementById("pdfViewer").src = pdfPath;
+              document.getElementById("pdfViewer").classList.remove("hidden");
+              document.getElementById("placeholderText").classList.add("hidden");
+            }
+          }
+          
        
 
        
